dd8f0cb8e1d1b0b600baef4dee3fe25a06646a3a
Kevin Tripp <ktripp1@gmail.com>
2017-03-22 08:31:34 -0400

    Added from submission and file submission

--- meadowlark.js
+++ meadowlark.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 
 var app = express();
-var handlebars = require('express-handlebars').create(
-  {defaultLayout:'main',
-  helpers: {
-    section: function(name, options){
-      if (!this._sections) this._sections={};
-      this._sections[name] = options.fn(this);
-      return null;
-      }
-  }
+
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 });
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/public"));
-app.use(require('body-parser').urlencoded({extended:true}));
+app.use(require('body-parser')());
 
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' && req.query.test == '1';
@@ -46,16 +51,30 @@ app.get('/newsletter',function(req, res){
   res.render('newsletter',{csrf:'csrf token goes here'});
 });
 
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+
 app.post('/process', function(req, res){
-  if(req.xhr || req.accepts('json,html')==='json'){
-    res.send({success:true});
-  }else{
-  console.log('form(from querystring): ' + req.query.form);
-  console.log('csrf token (from hidden form field): ' + req.body._csrf);
-  console.log('Name (from visible form field): ' + req.body.name);
-  console.log('Email (from visible form field): ' + req.body.email);
-  res.redirect(303, '/thank-you')
-}
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
 });
 
 // 404 catch all handler(middleware)
