1329373d9f944b7385e7b731c6c852e843722261
Kevin Tripp <ktripp1@gmail.com>
2017-03-27 18:47:23 -0400

    Added Flash Messages

--- meadowlark.js
+++ meadowlark.js
@@ -19,17 +19,42 @@ var handlebars = require('express-handlebars').create({
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
+
+var credentials = require('./credentials.js')
 
 app.use(express.static(__dirname + "/public"));
 app.use(require('body-parser')());
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+  resave:false,
+  saveUninitialized: false,
+  secret:credentials.cookieSecret,
+}));
+
 
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' && req.query.test == '1';
   next();
 });
 
+
+
+
+
+app.use( function (req, res, next){
+  //  if there's a flash message, transfer it to the context, then clear it
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
+
 app.get('/', function(req, res){
+  res.cookie('monster','nom nom');
+  res.cookie('signed_monster','nom nom',{singed:true});
+  req.session.userName = 'Anonymous';
+  var colorScheme = req.session.colorScheme || 'dark';
   res.render('home');
 });
 
@@ -49,6 +74,43 @@ app.get('/tours/request-group-rate', function(req, res){
 
 app.get('/newsletter',function(req, res){
   res.render('newsletter',{csrf:'csrf token goes here'});
+});
+
+var VALID_EMAIL_REGEX = new RegExp('^[ a-zA-Z0-9.!# $%&\'* +\/ =? ^_ `{ |} ~-] +@'
+  + '[ a-zA-Z0-9](?:[ a-zA-Z0-9-]{ 0,61}[ a-zA-Z0-9])?'
+  + '(?:\.[ a-zA-Z0-9](?:[ a-zA-Z0-9-]{ 0,61}[ a-zA-Z0-9])?) + $');
+
+app.post('/newsletter',function(req, res){
+  var name = req.body.name || "", email = req.body.email || '';
+  //input calidation
+  if(!email.match(VALID_EMAIL_REGEX)){
+    if(req.xhr) return res.json({error:'Invalid name email address.'});
+    req.session.flash = {
+      type:'danger',
+      intro: 'Validation Error!',
+      message: 'The email address you entered was not valid',
+    };
+    return res.redirect(303,'/newsletter/archive');
+  }
+  new NewsLetterSignUp({ name:name, email, email}).save(function(err){
+    if(err){
+      if (req.xhr) return res.json({error: ' Database Error.'});
+      req.session.flash = {
+        type:'danger',
+        intro: 'Database Error!',
+        message: 'There was a database error; please try again later',
+      }
+      return res.redirection(303, '/newsletter/archive');
+    }
+      if (req.xhr) return res.json({success: true});
+      req.session.flash = {
+        type:'success',
+        intro: 'Thank you! bob',
+        message: 'You have now been signed up for the newsletter',
+      };
+      return res.redirect(303,'/newsletter/archive');
+  });
+
 });
 
 app.get('/contest/vacation-photo', function(req, res){
